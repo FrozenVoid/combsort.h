8eab0d3560feba4631364df7280e537cf4d2793a
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-04-28 19:35:20 +0300

    Create combsort.h

---
+++ combsort.h
@@ -0,0 +1,19 @@
+#pragma once
+/* optimized combsort
+combsort(arr,elems) sort array from smallest to largest
+combsortr(arr,elems) sort array from largest to smallest
+Performance: ~160 cycles per int
+(about 1/4 of radix sort speed,except for small arrays(<140))
+*/
+#define arrcombsort(arr,elems,dir) ({size_t comb_i,comb_j, combgap=elems,comblen=combgap;\
+do{combgap = combgap<10 ? combgap-1 : ((combgap*796)>>10)+1;\
+ for (comb_i=0,comb_j=combgap; comb_j<comblen; comb_i++,comb_j++){\
+const typeof(arr[0]) comb_tmpi=arr[comb_i];\
+const typeof(arr[0]) comb_tmpj=arr[comb_j];\
+const int comb_cnd=comb_tmpi dir comb_tmpj;\
+arr[comb_i]=comb_cnd?comb_tmpj:comb_tmpi;\
+arr[comb_j]=comb_cnd?comb_tmpi:comb_tmpj;};}while (combgap);})
+
+#define combsort(arr,elems) arrcombsort(arr,elems,>)
+#define combsortr(arr,elems) arrcombsort(arr,elems,<)
+
